a122b22a1bb7bc81273e77c018db269e8a1e828b
JuanCastao11 <juanmanuelcastano11b@gmail.com>
2023-10-31 18:54:54 -0500

    form ready

--- gx7/src/pages/form.jsx
+++ gx7/src/pages/form.jsx
@@ -3,54 +3,71 @@ import React from 'react'
 function Form() {
   return (
     <>
-    <div className="form w-full">
-      <form action="" className=' w-full flex m-10 justify-center'>
-        <div className=' w-[40%]'>
-        <div className="">
-          <label htmlFor="nombre">Nombre Completo</label>
-          <input type="text" id='nombre' />
+    <div className="form w-full  ">
+  <form action="" className="flex justify-center m-10">
+    <div className="w-[40%] m-10 ">
+      <div className="mb-4">
+        <label htmlFor="nombre" className="block">Nombre Completo</label>
+        <input type="text" id="nombre" className="w-full border-2 rounded-md p-2" />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="correo" className="block">Correo Electronico</label>
+        <input type="text" id="correo" className="w-full border-2 rounded-md p-2" />
+      </div>
+      <div className="flex mb-4">
+        <label className="block mr-4">Sexo</label>
+        <div className="flex flex-col">
+          <label htmlFor="femenino" className="mb-1">
+            Femenino*
+            <input type="radio" id="femenino" className="mr-2" />
+          </label>
+          <label htmlFor="masculino">
+            Masculino*
+            <input type="radio" id="masculino" className="mr-2" />
+          </label>
         </div>
-        <div className=''>
-        <label htmlFor="correo">Correo Electronico</label>
-          <input type="text" id='correo' />
-        </div>
-        <div className=' flex  '>
-          <label htmlFor="">Sexo</label>
-        <div className=' flex flex-col'>
-        <label htmlFor="femenino">Femenino*</label>
-          <input type="radio" id='femenino' />
-          <label htmlFor="masculino">Masculino*</label>
-          <input type="radio" id='masculino' />
-        </div>
-        </div>
-        <div className=' '>
-          <label htmlFor="area">Area</label>
-        <select value="" name="" id="area" className=' w-[95%] border-2 outline-0  h-[40px] rounded-md' >
-        <option value="vacio" ></option>
-          <option value="Estudiante" >Estudiante</option>
-          <option value="Profesor" >Profesor</option>
+      </div>
+      <div className="mb-4">
+        <label htmlFor="area" className="block">Area</label>
+        <select id="area" className="w-full border-2 rounded-md p-2">
+          <option value="vacio"></option>
+          <option value="Estudiante">Estudiante</option>
+          <option value="Profesor">Profesor</option>
         </select>
-        </div>
-        <div className=''>
-          <label htmlFor="descripcion">Descripción</label>
-          <input type="textarea" id='descripcion' /> 
-        </div>
-        <div className=' flex'>
-          <label htmlFor="">Roles*</label>
-          <div className=' flex flex-col'>
-            <input type="checkbox" id='boletin' />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="descripcion" className="block">Descripción</label>
+        <textarea id="descripcion" className="w-full border-2 rounded-md p-2"></textarea>
+      </div>
+      <div className="flex mb-4">
+        <label className="block mr-4">Roles*</label>
+        <div className="flex flex-col">
+          <div className="mb-1">
+            <input type="checkbox" id="boletin" className="mr-2" />
             <label htmlFor="boletin">Deseo recibir boletin informativo</label>
-            <input type="checkbox" id='Profesional' />
+          </div>
+          <div className="mb-1">
+            <input type="checkbox" id="Profesional" className="mr-2" />
             <label htmlFor="Profesional">Profesional de proyectos - Desarrollador</label>
-            <input type="checkbox" id='Gerente estrategico' />
+          </div>
+          <div className="mb-1">
+            <input type="checkbox" id="Gerente estrategico" className="mr-2" />
             <label htmlFor="Gerente estrategico">Gerente estrategico</label>
-            <input type="checkbox" id='Auxiliar Administrativo' />
+          </div>
+          <div className="mb-1">
+            <input type="checkbox" id="Auxiliar Administrativo" className="mr-2" />
             <label htmlFor="Auxiliar Administrativo">Auxiliar Administrativo</label>
           </div>
         </div>
-        </div>
-      </form>
+      </div>
+      <div className="boton flex justify-center">
+      <button type="button" class="text-white bg-gradient-to-r w-32 from-blue-500 via-blue-600 to-blue-700
+     focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-4 mb-2 ">Enviar</button>
+      </div>
     </div>
+  </form>
+</div>
+
     </>
   )
 }

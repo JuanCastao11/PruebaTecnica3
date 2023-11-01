2e6083b037707eef159c438b5cb9a88c1fdad6e5
JuanCastao11 <juanmanuelcastano11b@gmail.com>
2023-10-31 20:00:49 -0500

    area

---
+++ gx7/src/context/areaContext.jsx
@@ -0,0 +1,39 @@
+import { newContext, useContext, useState } from 'react'
+import {AreasRequest} from '../api/areas';
+
+const areasContext = newContext();
+
+export const useAreas = () => {
+    const context = useContext(areasContext);
+    if(!context){
+        throw new Error('Error')
+    }
+    return context;
+}
+
+export function areasProvider({children}) {
+    const [areas, setAreas] = useState([]);
+
+    const mostrarAreas = async () => {
+        try {
+            const res = await AreasRequest();
+            const areasId = res.data.map((area) => ({ id: area.id, name: area.name }));
+            setAreas(areasId);
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+
+    return(
+        <areasContext.Provider value={{
+            areas, mostrarAreas
+        }}>
+
+            {children}
+        </areasContext.Provider>
+        )
+}
+
